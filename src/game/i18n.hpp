951c82d5e6b50d86bb324451624e876103a812ec
Sancho <szycikm@gmail.com>
2022-06-29 23:58:08 +0200

    remove useless endlines from warning msgs

--- src/game/i18n.hpp
+++ src/game/i18n.hpp
@@ -3,7 +3,7 @@
 // bruh, that's some next level i18n
 
 #define STR_WINDOW_TITLE "FoE: Remains (Repairpony's Edition)"
-#define STR_CURSOR_LOAD_IMG_ERR "Error loading custom cursor image resource (%s), fallback cursor will be used.\n"
-#define STR_CURSOR_LOAD_PIXELS_ERR "Error loading image data (%s), fallback cursor will be used.\n"
-#define STR_CURSOR_LOAD_FAIL "Error loading cursors.\n"
-#define STR_FONT_LOAD_FAIL "Error loading font.\n"
+#define STR_CURSOR_LOAD_IMG_ERR "Error loading custom cursor image resource (%s), fallback cursor will be used."
+#define STR_CURSOR_LOAD_PIXELS_ERR "Error loading image data (%s), fallback cursor will be used."
+#define STR_CURSOR_LOAD_FAIL "Error loading cursors."
+#define STR_FONT_LOAD_FAIL "Error loading font."
