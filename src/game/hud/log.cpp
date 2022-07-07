404568698c53088f680d66aba2848b20e6cd860b
Sancho <szycikm@gmail.com>
2022-07-07 23:53:12 +0200

    log: lil refactor, add TODOs

--- src/game/hud/log.cpp
+++ src/game/hud/log.cpp
@@ -17,22 +17,23 @@ void Log::updateFontSize()
 
 /**
  * Draws message log items.
+ * TODO this should be calculated every 1s or 0.25s or whatever, then only draw the items.
  */
 void Log::draw(sf::RenderWindow *window)
 {
-	uint x = 5; // constant offset from left, will be enough for CORNER_*_LEFT
-	uint y = 2; // constant offset from top, will be enough for CORNER_TOP_*
+	uint x = LOG_ANCHOR_PADDING_LEFT; // constant offset from left, will be enough for CORNER_*_LEFT
+	uint y = LOG_ANCHOR_PADDING_TOP; // constant offset from top, will be enough for CORNER_TOP_*
 	uint timesUpCnt = 0;
 	ScreenCorner anchor = this->settings->getScreenCorner(SETT_ANCHOR_LOG);
 
 	// initial offset from top/bottom
 	if (anchor == CORNER_BOTTOM_LEFT || anchor == CORNER_BOTTOM_RIGHT)
-		y = window->getSize().y - this->history.size() * FONT_SIZE_NORMAL_WITH_GAP - 2;
+		y = window->getSize().y - this->history.size() * FONT_SIZE_NORMAL_WITH_GAP - LOG_ANCHOR_NEG_PADDING_BOTTOM;
 
 	for (LogElementText &item : this->history)
 	{
 		if (anchor == CORNER_TOP_RIGHT || anchor == CORNER_BOTTOM_RIGHT)
-			x = window->getSize().x - item.getLocalBounds().width - 7;
+			x = window->getSize().x - item.getLocalBounds().width - LOG_ANCHOR_NEG_PADDING_RIGHT;
 
 		item.setPosition(x, y);
 		window->draw(item);
@@ -43,6 +44,7 @@ void Log::draw(sf::RenderWindow *window)
 			timesUpCnt++;
 	}
 
+	// remove items which were in the log for longer than LOG_ELEMENT_LIFE_TIME_S
 	for (uint i = 0; i < timesUpCnt; i++)
 		this->history.pop_front();
 }
