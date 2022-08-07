6dfcbdfe86bea328dda7a0d13b4dfca199a47e01
Sancho <szycikm@gmail.com>
2022-08-08 00:04:21 +0200

    added res mgr concept comment

--- src/game/resource_manager.hpp
+++ src/game/resource_manager.hpp
@@ -10,6 +10,27 @@ enum ImageName
 	_IMGS_CNT
 };
 
+/**
+ * The role of the ResourceManager is to basically avoid a situation where a resource
+ * (e.g. image, audio file, etc.) is loaded multiple times.
+ *
+ * Care must be taken to ensure that the required resources are loaded.
+ *
+ * One approach would be to load all available resources immediately, however this would result
+ * in needlessly high usage of memory and lots of resources left unused.
+ *
+ * A better approach works like this: each room defines resources it needs to use, e.g. it needs
+ * certain images for texturing blocks. When a location loads its list of rooms, it compiles all
+ * resources required by its rooms into a set. When a location is being loaded, it can request all
+ * needed resources from resource manager, which then loads resources that are currently missing.
+ * When loading next location, a different one than current one (excluding basecamps), we can
+ * unload resources loaded by previous location. We can also skip unloading entirely, however
+ * this should be implemented as a dev setting.
+ *
+ * There's also a group of resources that needs to be loaded all the time, e.g. fonts, some
+ * translations, cursors, etc. Those resources need to be marked as "core" to prevent unloading
+ * them at any point.
+ */
 class ResourceManager
 {
 	private:
