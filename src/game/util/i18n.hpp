505c57e09f41259e5d86eb11005d62b511c339d0
Sancho <szycikm@gmail.com>
2022-09-22 21:29:38 +0200

    changed title to RR

--- src/game/util/i18n.hpp
+++ src/game/util/i18n.hpp
@@ -2,7 +2,7 @@
 
 // bruh, that's some next level i18n
 
-#define STR_WINDOW_TITLE "FoE: Remains (Repairpony's Edition)"
+#define STR_WINDOW_TITLE "FoE: Remains of Remains"
 #define STR_CURSOR_LOAD_IMG_ERR "Error loading custom cursor image resource (%s), fallback cursor will be used."
 #define STR_CURSOR_LOAD_PIXELS_ERR "Error loading image data (%s), fallback cursor will be used."
 #define STR_CURSOR_LOAD_FAIL "Error loading cursors."
