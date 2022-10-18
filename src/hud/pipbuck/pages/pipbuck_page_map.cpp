6c60c5d7fc5503c8661a8585dc3aeebf920f8d42
Sancho <szycikm@gmail.com>
2022-10-19 00:21:51 +0200

    engineering notes

--- src/hud/pipbuck/pages/pipbuck_page_map.cpp
+++ src/hud/pipbuck/pages/pipbuck_page_map.cpp
@@ -5,6 +5,8 @@ PipBuckPageMap::PipBuckPageMap(ResourceManager &resMgr)
 	this->dummy.setFont(*resMgr.getFont(FONT_FIXED));
 	this->dummy.setPosition(500.f, 500.f);
 	this->dummy.setString("map");
+
+	// TODO if player enters the backstage, display on map should also change to backrooms. same for minimap
 }
 
 std::string PipBuckPageMap::getLabel()
