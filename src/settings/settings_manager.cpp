2088ed31fd0fdc5f26bba9b74e413622cde5382c
h67ma <szycikm@gmail.com>
2022-11-13 23:05:34 +0100

    refactoring: clean up settings definitions a bit

--- src/settings/settings_manager.cpp
+++ src/settings/settings_manager.cpp
@@ -18,38 +18,49 @@ SettingsManager::SettingsManager()
 {
 	this->settings.resize(_SETTINGS_CNT);
 
-	// window
+	// +++++ window +++++
+
 	this->settings[SETT_FULLSCREEN_ENABLED].setupBool("fullscreen_enabled", false);
 	this->settings[SETT_FPS_LIMIT_ENABLED].setupBool("fps_limit_enabled", true);
 	this->settings[SETT_FPS_LIMIT].setupUint("fps_limit", 60);
 	this->settings[SETT_FAKE_VSYNC_ENABLED].setupBool("fake_vsync_enabled", false);
 	// TODO? windowed size + position override
 
-	// hud
+	// +++++ hud +++++
+
 #ifdef __linux__
 	// currently custom cursor (with color support) is broken on linux
 	this->settings[SETT_PREFER_CUSTOM_CURSOR].setupBool("prefer_custom_cursor", false);
 #else
 	this->settings[SETT_PREFER_CUSTOM_CURSOR].setupBool("prefer_custom_cursor", true);
 #endif /* __linux__ */
+
 	this->settings[SETT_SHOW_FPS_COUNTER].setupBool("show_fps_counter", true);
 	this->settings[SETT_ANCHOR_LOG].setupScreenCorner("log_anchor", CORNER_TOP_RIGHT);
 	this->settings[SETT_ANCHOR_FPS].setupScreenCorner("fps_anchor", CORNER_TOP_LEFT);
 	this->settings[SETT_GUI_SCALE].setupGuiScale("gui_scale", GUI_NORMAL);
-	this->settings[SETT_HUD_COLOR].setupColor("hud_color", sf::Color(0, 255, 153)); // default is greenish, same as in Remains
+
+	// default is greenish, same as in Remains
+	this->settings[SETT_HUD_COLOR].setupColor("hud_color", sf::Color(0, 255, 153));
+
 	//this->settings[SETT_LOG_MSG_TIMEOUT].setup("log_msg_timeout", 3); // is this really necessary?
 
-	// audio
-	this->settings[SETT_FX_VOLUME].setupUint("fx_volume", 100, [](uint val){ return val <= 100; }); // 100 is max volume
+	// +++++ audio +++++
 
-	// debug
+	// 100 is max volume
+	this->settings[SETT_FX_VOLUME].setupUint("fx_volume", 100, [](uint val){ return val <= 100; });
+
+	// +++++ debug +++++
 	// TODO maybe we could save window w&h on program exit and then restore it?
-	this->settings[SETT_WINDOW_WIDTH].setupUint("window_w", 1280, [](uint val){ return val <= 7680; }); // let's be realistic about max value
-	this->settings[SETT_WINDOW_HEIGHT].setupUint("window_h", 720, [](uint val){ return val <= 7680; }); // let's be realistic about max value
+
+	// let's be realistic about max window size
+	this->settings[SETT_WINDOW_WIDTH].setupUint("window_w", 1280, [](uint val){ return val <= 7680; });
+	this->settings[SETT_WINDOW_HEIGHT].setupUint("window_h", 720, [](uint val){ return val <= 7680; });
+
 	this->settings[SETT_WRITE_LOG_TO_FILE].setupBool("write_log_to_file", true);
 	this->settings[SETT_PRINT_MSGS].setupBool("print_msgs_cout", false);
 	this->settings[SETT_VERBOSE_DEBUG].setupBool("verbose_debug", false);
-	// TODO this->settings[SETT_SHOW_BOUNDING_BOXEN].setupBool("show_bounding_boxen", false); // Brian, what's the plural form of "box"?
+	// TODO this->settings[SETT_SHOW_BOUNDING_BOXEN].setupBool("show_bounding_boxen", false);
 	// TODO this->settings[SETT_DEV_CONSOLE_ENABLED].setupBool("dev_console_enabled", false);
 }
 
