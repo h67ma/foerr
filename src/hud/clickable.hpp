9e74efedf5948a4e8f25fd2336d51a6d27106f7e
h67ma <szycikm@gmail.com>
2022-11-16 21:08:55 +0100

    added an insightful comment

--- src/hud/clickable.hpp
+++ src/hud/clickable.hpp
@@ -5,8 +5,8 @@
 enum ClickStatus
 {
 	CLICK_NOT_CONSUMED,
-	CLICK_CONSUMED,
-	CLICK_CONSUMED_CLOSE,	// special status used to signal that PipBuck should be closed when a special child button was pressed
+	CLICK_CONSUMED,			// omnomnom
+	CLICK_CONSUMED_CLOSE,	// special status used to signal that PipBuck should be closed
 	CLICK_CONSUMED_UNLOAD,	// special status used to signal that PipBuck should unload its campaign infos
 };
 
