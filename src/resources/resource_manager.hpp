d0cd42df10d5d5d097cf7cfe23147f857a503b40
h67ma <szycikm@gmail.com>
2023-01-09 00:05:18 +0100

    updated res mgr doc

--- src/resources/resource_manager.hpp
+++ src/resources/resource_manager.hpp
@@ -25,49 +25,36 @@ enum FontType
 };
 
 /**
- * The role of the ResourceManager is to basically avoid a situation where a resource
- * (e.g. texture, audio file, etc.) is loaded multiple times.
+ * The role of the ResourceManager is to avoid resource (e.g. textures, audio files, etc.) duplication in memory.
  *
- * Care must be taken to ensure that the required resources are loaded.
+ * Care must be taken to ensure that the required resources are loaded, but also that we don't keep unused resources
+ * loaded.
  *
- * One approach would be to load all available resources immediately, however this would result
- * in needlessly high usage of memory and lots of resources left unused in most cases.
+ * The current approach works like this: various elements everywhere in the game define the resources they need - e.g.
+ * certain textures to draw self. These elements can request the needed resources from Resource Manager, which will
+ * load them if they are not already loaded, and return a shared pointer. Elements using such resources must store a
+ * shared_ptr to the desired resource, to prevent it from being deallocated. Utility classes such as SpriteResource are
+ * provided to make it clear that certain resource is managed by ResourceManager.
  *
- * A better approach works like this: each room defines resources it needs to use, e.g. it needs
- * certain textures for texturing cells. When a location loads its list of rooms, it compiles all
- * resources required by its rooms into a set. When a location is being loaded, it can request all
- * needed resources from resource manager, which then loads resources that are currently missing.
- * When loading next location, a different one than current one (excluding basecamps), we can
- * unload resources loaded by previous location. We can also skip unloading entirely, however
- * this should be implemented as a dev setting, if at all (TODO?).
+ * Deallocation of resources can happen e.g. when the player changes locations and game decides to unload the old
+ * location. Resource Manager then goes through all managed resources (::cleanUnused()) and checks which ones are unused
+ * (via shared_ptr usage count).
  *
- * Shared pointers are used to keep track of different resources. This way the Resource Manager
- * does not own the resources, but rather provides unified access to them and prevents duplication.
- * Because actual usage of resources is tracked by shared pointers, we can easily remove unused
- * resources, e.g. when jumping between locations, or when unloading a campaign.
+ * Resource Manager does not store text (json) resources - they should be loaded by specialized classes which validate
+ * their structure and provide convenient methods for getting/modifying data.
  *
- * Care must be taken to ensure that resources that are being used are not deallocated.
- * It is recommended to use the correct wrapper class (SoundResource/SpriteResource), or store
- * a shared pointer to the needed resource as a private member, in addition to passing it to
- * some SFML object (because SFML objects typically use references, so shared ptr usage count
- * would be lost).
+ * Resource files are identified by their path in filesystem. The same string is used for loading and getting resources.
  *
- * Resource manager does not store text (json) resources.
+ * There's a group of resources that need to be loaded all the time, e.g. some textures. Paths of these resources are
+ * hardcoded. They must be loaded before starting the game. Game should not continue if any of those "core" resources
+ * is missing.
  *
- * Resource files are identified by their path in filesystem. The same string is used for loading
- * and getting resources.
- *
- * There's a group of resources that need to be loaded all the time, e.g. some textures.
- * Paths of these resources are hardcoded. They must be loaded before starting the game.
- * Game should not continue if any of those "core" resources is missing.
- *
- * Fonts are special cases of core resources, i.e. they are stored differently (in arrays)
- * and are never unloaded, therefore their usage is not tracked with smart pointers.
+ * Fonts are special cases of core resources, i.e. they are stored differently (in an array), and are never unloaded,
+ * therefore their usage is not tracked with smart pointers.
  *
  * Cursors are handled separately via the CursorManager class.
  *
- * TODO? res mgr could potentially be made into a "static class", same as with Log, to avoid
- * passing it along everywhere.
+ * TODO? res mgr could potentially be made into a "static class", same as with Log, to avoid passing it along everywhere
  */
 class ResourceManager
 {
