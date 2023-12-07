acaad650bda3448831d3503fdb2e921ade2355d4
h67ma <szycikm@gmail.com>
2023-12-07 23:16:09 +0100

    build: Make git version script more generic
    Put the bulk of git version script into a reusable function (will be
    useful e.g. for packaging). To use this function, include
    CMakeGitVersion.cmake. Output will be in the $GIT_VERSION variable.
    "gitversion" target doesn't require any changes.
    
    Signed-off-by: h67ma <szycikm@gmail.com>

--- CMakeGitVersion.cmake
+++ CMakeGitVersion.cmake
@@ -1,21 +1,28 @@
 # based on https://github.com/nocnokneo/cmake-git-versioning-example
-if(GIT_EXECUTABLE)
-	get_filename_component(SRC_DIR ${SRC} DIRECTORY)
-	execute_process(
-		COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --match "v*"
-		WORKING_DIRECTORY ${SRC_DIR}
-		OUTPUT_VARIABLE GIT_DESCRIBE_VERSION
-		RESULT_VARIABLE GIT_DESCRIBE_ERROR_CODE
-		OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
-	if(NOT GIT_DESCRIBE_ERROR_CODE)
-		set(GIT_VERSION ${GIT_DESCRIBE_VERSION})
+function(get_git_version GIT_EXECUTABLE SRC_DIR)
+	if(GIT_EXECUTABLE)
+		execute_process(
+			COMMAND ${GIT_EXECUTABLE} describe --tags --dirty --match "v*"
+			WORKING_DIRECTORY ${SRC_DIR}
+			OUTPUT_VARIABLE GIT_DESCRIBE_VERSION
+			RESULT_VARIABLE GIT_DESCRIBE_ERROR_CODE
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		if(NOT GIT_DESCRIBE_ERROR_CODE)
+			set(_GIT_VERSION ${GIT_DESCRIBE_VERSION})
+		endif()
 	endif()
-endif()
 
-if(NOT DEFINED GIT_VERSION)
-	set(GIT_VERSION v0.0.0-unknown)
-	message(WARNING "Failed to determine GIT_VERSION. Using default version: \"${GIT_VERSION}\".")
-endif()
+	if(NOT DEFINED _GIT_VERSION)
+		set(_GIT_VERSION v0.0.0-unknown)
+		message(WARNING "Failed to determine GIT_VERSION. Using default version: \"${_GIT_VERSION}\".")
+	endif()
 
-configure_file(${SRC} ${DST} @ONLY)
+	set(GIT_VERSION ${_GIT_VERSION} PARENT_SCOPE)
+endfunction()
+
+if(DEFINED SRC)
+	get_filename_component(SRC_DIR ${SRC} DIRECTORY)
+	get_git_version(${GIT_EXECUTABLE} ${SRC_DIR})
+	configure_file(${SRC} ${DST} @ONLY)
+endif()
