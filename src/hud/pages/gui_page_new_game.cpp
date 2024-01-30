h67ma/foerr

commit 7f0168ec3e2df3dccffb1b7ba629ff6d2bbb74c0

foerr: Deselect main menu item after selecting campaign
Reset main menu to initial state (no page selected) upon selecting a
campaign.

Signed-off-by: h67ma <szycikm@gmail.com>

src/hud/pages/gui_page_new_game.cpp
@@ -180,7 +180,7 @@ ClickStatus GuiPageNewGame::handleLeftClick(sf::Vector2i clickPos)
 				this->rebuildCampaignList();
 			}
 
-			return CLICK_CONSUMED;
+			return CLICK_CONSUMED_RESET_MENU;
 		}
 	}
 

src/hud/clickable.hpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 //
-// (c) 2022-2023 h67ma <szycikm@gmail.com>
+// (c) 2022-2024 h67ma <szycikm@gmail.com>
 
 #pragma once
 
@@ -21,6 +21,7 @@ enum ClickStatus
 	CLICK_CONSUMED_CLOSE, // PipBuck should be closed
 	CLICK_CONSUMED_UNLOAD, // PipBuck should unload its campaign infos
 	CLICK_CONSUMED_SETTINGS_CHANGED, // components should update their state after settings change
+	CLICK_CONSUMED_RESET_MENU, // parent component should reset menu/list to initial state
 };
 
 class Clickable

src/hud/main_menu/main_menu.cpp
@@ -176,6 +176,13 @@ ClickStatus MainMenu::handleLeftClick(sf::Vector2i clickPos)
 		if (status != CLICK_NOT_CONSUMED)
 		{
 			this->btnSound.play();
+
+			if (status == CLICK_CONSUMED_RESET_MENU)
+			{
+				this->changePage(MAINM_PAGE_NONE);
+				return CLICK_CONSUMED;
+			}
+
 			return status;
 		}
 	}
@@ -191,8 +198,7 @@ ClickStatus MainMenu::handleLeftClick(sf::Vector2i clickPos)
 			// if there's no associated page, ::changePage() will automatically deselect page, so e.g. when player
 			// selects "Settings" page, then clicks "Continue", plays the game a bit, and comes back to main menu, it
 			// will visually be in its initial state (no page selected), which is exactly what we want to happen.
-			// TODO? probably there will be other cases of this, where game state change initiated from inside a page
-			// ("Load" page). we'd need to somehow deselect page then.
+			// menu item can also be deselected by returning CLICK_CONSUMED_RESET_MENU in a page (see above).
 			this->changePage(btn.first);
 
 			this->btnSound.play();

commit db32d951900ad2020a13ab6b003aa1a95a23322d

foerr: Fix menu resetting on campaign load fail
Don't close the New Game page in main menu when campaign fails to load.

Fixes: 7f0168ec3e2d ("foerr: Deselect main menu item after selecting campaign")
Signed-off-by: h67ma <szycikm@gmail.com>

src/hud/pages/gui_page_new_game.cpp
@@ -178,6 +178,7 @@ ClickStatus GuiPageNewGame::handleLeftClick(sf::Vector2i clickPos)
 				// removed. refresh campaign list in an attempt to reflect any changes
 				Log::w(STR_REFRESHING_CAMPAIGN_LIST);
 				this->rebuildCampaignList();
+				return CLICK_CONSUMED;
 			}
 
 			return CLICK_CONSUMED_RESET_MENU;